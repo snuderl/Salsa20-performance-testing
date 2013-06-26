d518ffaed5704cd7015845a3316735e67101981c
Blaz Snuderl <snuderl@gmail.com>
2013-06-26 13:39:54 +0200

    ab

--- test.c
+++ test.c
@@ -79,7 +79,7 @@ void test_run(int messageSize, int runs){
   stopm = clock();
   int clocks = PRINTTIME
 
-  printf( "---- Encryption | %f seconds | %d bytes.\n", clocks/((double)runs*messageSize), messageSize);
+  printf( "---- Encryption | %f seconds | %d bytes.\n", clocks/((double)runs*CLOCKS_PER_SEC), messageSize);
 
   startm = clock();
   for(loop=0;loop<runs;loop++){
